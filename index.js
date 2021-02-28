d2c81470b9c1a5df436e91c44d9970b07c2e681e
redolent-history-c933 <nathoff22@gmail.com>
2021-02-28 20:22:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ function add(){
 function subtract(){ 60 - 40}
 function multiply(){2 * 3.40}
 function divide(){5.0 / 2.5}
+
+function add(a, b){return a + b}
+function subtract(a, b){return a - b}
+function multiply(a, b) {return a * b}
+function divide(a, b){return a/b}
+
 function increment (n){
   return n++
 }
-function decrement (n) {n--}
+function decrement (n) {return n--}
